8d79da5fe9fac25fe3d59290a81373699b70cc21
Adrian Jablonski <Adrian.jablonski19@gmail.com>
2019-06-16 13:12:17 -0500

    Updated notes

--- notes/sqlnotes.sql
+++ notes/sqlnotes.sql
@@ -1,10 +1,11 @@
+-- ##### Selecting multiple values in the same column #####
 SELECT * FROM users WHERE id IN (1, 2, 4);
 -- Is the same as writing
 SELECT * FROM users WHERE id = 1 OR id = 2 OR id = 4;
 
 
--- TRANSACTIONS - Switches autocommit off to prevent unwanted errors and test SQL statements without affecting the live database
-BEGIN TRANSACTION;
+-- ##### Creating transactions to test SQL statements before updating live database #####
+BEGIN TRANSACTION; -- TRANSACTIONS - Switches autocommit off to prevent unwanted errors and test SQL statements without affecting the live database
 
 COMMIT; -- Used to commit changes to database that are in the current transaction. This will make the changes permanent
 
